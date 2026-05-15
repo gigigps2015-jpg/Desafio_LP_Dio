19cb8ef5627595267cadfdc8c4fd724260c5770e
Gislene A. Sousa e S. <gigigps2015@gmail.com>
2026-05-15 16:35:05 -0300

    index02

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let name = "gislene S2";
 let idade = 38
 let ondeMora = "CMD- MG ";
+let trabalho = "logistica de transporte";
 console.log(" Ola, meu nome é " + name + ", tenho " + idade + " anos e moro em " + ondeMora);
+console.log (" Trabalho como " + trabalho);
