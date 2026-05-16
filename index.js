ed2b87e2f89d9a0aec01e430fae27afa277b6425
Gislene A. Sousa e S. <gigigps2015@gmail.com>
2026-05-16 08:38:15 -0300

    Entendendo o Desafio -  level classification based on XP
    Entendendo o Desafio
    >Instruções para entrega

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 
-let nomeHeroi = "FeliciaMaldosa";
-let xp = 90000;
-let nivel = "";
+# 1️⃣ Desafio Classificador de nível de Herói
 
+**O Que deve ser utilizado**
 
-if (xp <= 1000) {
-    nivel = "Ferro";
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
 
-} else if (xp <= 2000) {
-    nivel = "Bronze";
+## Objetivo
 
-} else if (xp <= 5000) {
-    nivel = "Prata";
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
 
-} else if (xp <= 7000) {
-    nivel = "Ouro";
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 5.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000= Imortal
+Se XP for maior ou igual a 10.001 = Radiante
 
-} else if (xp <= 8000) {
-    nivel = "Platina";
+## Saída
 
-} else if (xp <= 9000) {
-    nivel = "Ascendente";
-
-} else if (xp <= 10000) {
-    nivel = "Imortal";
-
-} else {
-    nivel = "Radiante";
-}
-
-console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
+Ao final deve se exibir uma mensagem:
+"O Herói de nome **{nome}** está no nível de **{nivel}**"
